184ace5e23d5e9bcc924ea04a4551e7302b38dab
songjj <songjj@zetyun.com>
2023-05-16 15:33:03 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,58 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+<div>
+  <h2>Vue3的生命周期</h2>
+  <div>
+    <button @click="updateContent">点击更改组件内容</button>
+    <button @click="show">创建和销毁组件</button>
+    <div ref="demo">{{ content }}</div>
+    <AboutMe v-if="display" ></AboutMe>
+  </div>
+</div>
 </template>
 
+<script setup lang='ts'>
+import { ref, onBeforeMount, onMounted, onBeforeUpdate, onUpdated } from 'vue'
+// import AboutMe from './views/AboutMe.vue'
+import AboutMe from './views/AboutMe.vue'
+
+console.log("setup语法糖模式中，可以直接在这里当做created函数使用");
+
+const demo = ref<HTMLDivElement>()
+
+const content = ref<string>("这是内容：div内容")
+
+const display = ref<boolean>(true)
+
+const updateContent = ()=>{
+  content.value = "div组价内容更新"
+}
+
+const show = ()=>{
+  display.value = !display.value
+}
+
+// 页面渲染之前
+onBeforeMount(() => {
+  console.log("渲染之前", demo);
+})
+
+// 页面渲染之后
+onMounted(() => {
+  console.log("渲染之后", demo);
+})
+
+// 组件更新之前
+onBeforeUpdate(() => {
+  console.log("组件更新之前", demo);
+})
+
+// 组件更新之后
+onUpdated(() => {
+  console.log("组件更新之后", demo);
+})
+
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
---
+++ src/views/AboutMe.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+这是关于页面的内容
+</div>
+</template>
+
+<script setup lang='ts'>
+import {onBeforeUnmount, onUnmounted } from 'vue'
+// 组件销毁之前
+onBeforeUnmount(() => {
+  console.log("组件销毁之前");
+})
+
+// 组件销毁之后
+onUnmounted(() => {
+  console.log("组件销毁之后");
+})
+</script>
+
+<style scoped>
+
+</style>
